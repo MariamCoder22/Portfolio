2c9098cab38629a94b6a77e783e1d963ecc74ec4
WerisCoder <humphbur32@gmail.com>
2022-11-18 09:38:45 -0600

    first commit

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default[
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/rm8.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/deathnote.mp4",
+    }
+]
